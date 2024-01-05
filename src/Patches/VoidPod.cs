4d7a85bffe7a2474d37f0dddcbeb8ce8e451f05d
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-01-05 21:00:34 +1100

    Create VoidPod.cs

---
+++ src/Patches/VoidPod.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RoR2;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+
+namespace Experimental
+{
+    [HarmonyPatch]
+    internal static class VoidPod
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(CharacterBody), nameof(CharacterBody.Awake))]
+        private static void ReplacePreferredPodPrefab(CharacterBody __instance)
+        {
+            GameObject prefab = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidSurvivor/VoidSurvivorBody.prefab").WaitForCompletion();
+            CharacterBody body = prefab?.GetComponent<CharacterBody>();
+            // __instance.preferredPodPrefab = body.preferredPodPrefab;
+            __instance.preferredPodPrefab = null;
+        }
+    }
+}
