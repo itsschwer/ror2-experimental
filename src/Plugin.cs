f6f7a1e646401ee1fcbe55a32d9528c76a0e67f9
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-08-14 20:49:20 +1000

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,21 @@
+using BepInEx;
+
+namespace Eater
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = Author + "." + Name;
+        public const string Author = "itsschwer";
+        public const string Name = "eater";
+        public const string Version = "0.0.0";
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity Message")]
+        private void Awake()
+        {
+            Log.Init(Logger);
+
+            Log.Message("~awake.");
+        }
+    }
+}
