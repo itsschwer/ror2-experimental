365e72eb53a96b4fc51880709801d0405318a1b7
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-08-24 22:59:26 +1000

    Eat consecutive success sound from LetsGoGambling

--- src/IL/LetsGoGamblingSuccessSoundEater.cs
+++ src/IL/LetsGoGamblingSuccessSoundEater.cs
@@ -7,6 +7,12 @@ namespace Eater.IL
 {
     internal static class LetsGoGamblingSuccessSoundEater
     {
+        private const int OPCODE_COUNT = 9;
+
+        private const int gambling_awdangit = 51628376;
+        private const int gambling_awyeahyeah = 853644935;
+        private const int gambling_cantstopwinning = 444218535;
+
         internal static void Apply()
         {
             if (!BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(LetsGoGamblingPlugin.MODUID)) return;
@@ -14,12 +20,16 @@ namespace Eater.IL
             MethodInfo method = typeof(LetsGoGamblingPlugin).GetMethod("ShrineChanceBehavior_AddShrineStack", BindingFlags.Instance | BindingFlags.NonPublic);
             ILHook hook = new(method, (il) => {
                 ILCursor c = new(il);
+
+                // NetMessageExtensions.Send((INetMessage)(object)new EmitSoundAtPoint(444218535u, self.gameObject.transform.position), (NetworkDestination)1);
                 c.GotoNext(
-                    // if (successfulPurchaseCount != successfulPurchaseCount2)
-                    //     NetMessageExtensions.Send((INetMessage)(object)new EmitSoundAtPoint(853644935u, self.gameObject.transform.position), (NetworkDestination)1);
-                    x => x.MatchLdcI4(853644935) // IL_002e
-                );
-                c.RemoveRange(9); // Skip to end of if block (past IL_004f)
+                    x => x.MatchLdcI4(gambling_cantstopwinning)
+                ).RemoveRange(OPCODE_COUNT);
+
+                // NetMessageExtensions.Send((INetMessage)(object)new EmitSoundAtPoint(853644935u, self.gameObject.transform.position), (NetworkDestination)1);
+                c.GotoNext(
+                    x => x.MatchLdcI4(gambling_awyeahyeah)
+                ).RemoveRange(OPCODE_COUNT);
 
                 Log.Info($"Applied ILHook to {method.DeclaringType.FullName}.{method.Name}");
             });
