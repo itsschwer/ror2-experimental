70e87b71baff660ca9079841ce724ef9f16aa55c
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-01 00:28:54 +1000

    Just use Exception.ToString() (near identical)

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -24,28 +24,9 @@ namespace SprintingOnTheScoreboard
             }
             catch (HarmonyLib.HarmonyException e) when (e.InnerException?.InnerException?.InnerException != null && e.InnerException.InnerException.InnerException.Message.Contains("Parameter \"onlyAllowMovement\" not found in method static bool RoR2.PlayerCharacterMasterController::CanSendBodyInput")) {
                 // HarmonyLib.HarmonyException: IL Compile Error (unknown location) ---> HarmonyLib.HarmonyException: IL Compile Error (unknown location) ---> HarmonyLib.HarmonyException: IL Compile Error (unknown location) ---> System.Exception: Parameter "onlyAllowMovement" not found
-                Logger.LogError(ReplicateUnityLogException(e)); // Keep the big scary red stack trace for perceptibility, but use plugin log source instead of Unity Log to better indicate source
+                Logger.LogError(e); // Keep the scary big red stack trace for perceptibility but use plugin log source (instead of Unity Log)
                 Logger.LogWarning("Failed to patch! This mod has no effect on versions prior to the Seekers Of The Storm patch.");
             }
-        }
-
-        private static string ReplicateUnityLogException(System.Exception exception)
-        {
-            System.Collections.Generic.Stack<System.Exception> exceptions = new();
-            while (exception != null) {
-                exceptions.Push(exception);
-                exception = exception.InnerException;
-            }
-
-            System.Text.StringBuilder sb = new();
-            System.Exception top = exceptions.Pop();
-            sb.AppendLine($"{top.GetType().FullName}: {top.Message}").AppendLine("Stack trace:").AppendLine(top.StackTrace);
-            while (exceptions.Count > 0) {
-                System.Exception e = exceptions.Pop();
-                sb.AppendLine($"Rethrow as {e.GetType().FullName}: {e.Message}").AppendLine(e.StackTrace);
-            }
-
-            return sb.ToString();
         }
     }
 }
