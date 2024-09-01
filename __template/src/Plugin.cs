d8a593f0ba0bb3380705bb95856e45488316ba79
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-01 10:27:39 +1000

    Create template

---
+++ __template/src/Plugin.cs
@@ -0,0 +1,25 @@
+using BepInEx;
+using RoR2;
+
+namespace PLUGIN_NAME
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = Author + "." + Name;
+        public const string Author = "itsschwer";
+        public const string Name = "PLUGIN_NAME";
+        public const string Version = "0.0.0";
+
+        public static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
+
+        private void Awake()
+        {
+            // Use Plugin.GUID instead of Plugin.Name as source name
+            BepInEx.Logging.Logger.Sources.Remove(base.Logger);
+            Logger = BepInEx.Logging.Logger.CreateLogSource(Plugin.GUID);
+
+            Logger.LogMessage("~awake.");
+        }
+    }
+}
