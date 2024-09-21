1af1c8d6a7096c7cf121745e3c63eb02545a600a
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-22 01:16:10 +1000

    [name] Apply when lobby is created

--- namechanger/src/Config.cs
+++ namechanger/src/Config.cs
@@ -11,17 +11,14 @@ namespace NameChanger
 
         public Config(ConfigFile config)
         {
+            ModSettingsManager.AddOption(new GenericButtonOption("", "Settings", "Force user names to update, in case the name replacement is not applied.", "Refresh User Names", Plugin.UpdateUserNames));
+
             nameReplacement = config.Bind<string>("Settings", nameof(nameReplacement), "",
                 "Input a name to use as a replacement for the name that appears in-game (excluding the lobby list / user connected messages).\n\nLeave empty to disable user name replacement.");
             ModSettingsManager.AddOption(new StringInputFieldOption(nameReplacement));
             nameReplacement.SettingChanged += NameReplacement_SettingChanged;
         }
 
-        private void NameReplacement_SettingChanged(object sender, System.EventArgs e)
-        {
-            foreach (RoR2.NetworkUser user in RoR2.NetworkUser.readOnlyInstancesList) {
-                user.UpdateUserName();
-            }
-        }
+        private void NameReplacement_SettingChanged(object sender, System.EventArgs e) => Plugin.UpdateUserNames();
     }
 }
--- namechanger/src/Plugin.cs
+++ namechanger/src/Plugin.cs
@@ -1,7 +1,10 @@
 using BepInEx;
+using HarmonyLib;
+using RoR2;
 
 namespace NameChanger
 {
+    [HarmonyPatch]
     [BepInDependency(RiskOfOptions.PluginInfo.PLUGIN_GUID)]
     [BepInPlugin(GUID, Name, Version)]
     public sealed class Plugin : BaseUnityPlugin
@@ -23,9 +26,30 @@ namespace NameChanger
 
             Config = new Config(base.Config);
 
-            new HarmonyLib.Harmony(Info.Metadata.GUID).PatchAll();
+            new Harmony(Info.Metadata.GUID).PatchAll();
 
             Logger.LogMessage("~awake.");
         }
+
+        public static void UpdateUserNames() {
+            foreach (NetworkUser user in NetworkUser.readOnlyInstancesList) {
+                user.UpdateUserName();
+            }
+        }
+
+
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(NetworkUser), nameof(NetworkUser.UpdateUserName))]
+        private static void NetworkUser_UpdateUserName(NetworkUser __instance)
+        {
+            bool isClient = __instance.isLocalPlayer;
+            if (isClient && !string.IsNullOrWhiteSpace(Plugin.Config.NameReplacement)) {
+                __instance.userName = Plugin.Config.NameReplacement;
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(LocalUser), nameof(LocalUser.LinkNetworkUser))]
+        private static void LocalUser_LinkNetworkUser() => Plugin.UpdateUserNames();
     }
 }
