7b91fed8cfa8c61f94a1fad49b599a1930add336
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-26 17:38:01 +1000

    [eater] Add UnityExplorerEater

--- eater/src/Plugin.cs
+++ eater/src/Plugin.cs
@@ -26,6 +26,7 @@ namespace Eater
 #if LETSGOGAMBLING
             LetsGoGamblingSuccessSoundEater.Apply();
 #endif
+            UnityExplorerEater.Apply();
 
             Logger.LogMessage("~awake.");
         }
---
+++ eater/src/UnityExplorerEater.cs
@@ -0,0 +1,20 @@
+﻿namespace Eater
+{
+    internal static class UnityExplorerEater
+    {
+        internal static void Apply()
+        {
+            On.RoR2.UI.SurvivorIconController.GetLocalUser += SurvivorIconController_GetLocalUser;
+        }
+
+        private static RoR2.LocalUser SurvivorIconController_GetLocalUser(On.RoR2.UI.SurvivorIconController.orig_GetLocalUser orig, RoR2.UI.SurvivorIconController self)
+        {
+            try {
+                return orig(self);
+            }
+            catch (System.InvalidCastException) {
+                return RoR2.LocalUserManager.GetFirstLocalUser();
+            }
+        }
+    }
+}
