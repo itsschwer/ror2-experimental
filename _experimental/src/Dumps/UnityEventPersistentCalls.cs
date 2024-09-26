a2840358f71acff5370541c3998f8a52613bb358
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-09-26 16:53:50 +1000

    [exp] Update UnityEventPersistentCalls.cs
    forgot to commit

--- _experimental/src/Dumps/UnityEventPersistentCalls.cs
+++ _experimental/src/Dumps/UnityEventPersistentCalls.cs
@@ -7,7 +7,7 @@ namespace Experimental.Dumps
         public static string Dump(UnityEngine.Events.UnityEventBase unityEvent)
         {
             StringBuilder sb = new();
-            sb.AppendLine(unityEvent.ToString());
+            sb.AppendLine($"{unityEvent} ({unityEvent.m_Calls.m_RuntimeCalls.Count} non-persistent)");
             for (int i = 0; i < unityEvent.GetPersistentEventCount(); i++) {
                 sb.AppendLine($"\t[{i}] {unityEvent.GetPersistentMethodName(i)} | {unityEvent.GetPersistentTarget(i)}");
                 sb.AppendLine($"\t\t{unityEvent.m_PersistentCalls.GetListener(i).arguments.stringArgument}");
