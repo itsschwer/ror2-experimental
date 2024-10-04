b6b11902bc4958ca4ee44bf8b4aea6e1b067f12b
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-04 16:29:32 +1000

    [exp] Create TeleportHelperTrace

---
+++ _experimental/src/Patches/TeleportHelperTrace.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RoR2;
+
+namespace Experimental.Patches
+{
+    [HarmonyPatch]
+    internal static class TeleportHelperTrace
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(TeleportHelper), nameof(TeleportHelper.TeleportGameObject), [typeof(UnityEngine.GameObject), typeof(UnityEngine.Vector3)])]
+        private static void TeleportHelper_TeleportGameObject(UnityEngine.Vector3 newPosition)
+        {
+            string trace = new System.Diagnostics.StackTrace().ToString();
+            if (!trace.Contains("RoR2.MapZone.TeleportBody")) {
+                Plugin.Logger.LogWarning($"{nameof(TeleportHelper_TeleportGameObject)}> {newPosition}\n{new System.Diagnostics.StackTrace()}");
+            }
+        }
+    }
+}
