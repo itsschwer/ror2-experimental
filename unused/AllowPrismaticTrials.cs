7a8efa3f189350295da1781b44bc17f9ba4cc12a
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-13 09:59:33 +1100

    Create junk/unused folder

---
+++ unused/AllowPrismaticTrials.cs
@@ -0,0 +1,20 @@
+﻿#if DEBUG
+using HarmonyLib;
+using RoR2;
+
+namespace itsschwer.Junk
+{
+    /// <remarks>
+    /// <see href="https://github.com/Moffein/RiskyFixes/blob/061e5b91013f377c381e36ce99f1a260adef4884/RiskyFixes/Fixes/General/ModdedPrismaticTrials.cs"/>
+    /// </remarks>
+    [HarmonyPatch]
+    internal static class AllowPrismaticTrials
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(WeeklyRun), nameof(WeeklyRun.ClientSubmitLeaderboardScore))]
+        private static bool WeeklyRun_ClientSubmitLeaderboardScore() => false;
+
+        [HarmonyPrefix, HarmonyPatch(typeof(DisableIfGameModded), nameof(DisableIfGameModded.OnEnable))]
+        private static bool DisableIfGameModded_OnEnable() => false;
+    }
+}
+#endif
