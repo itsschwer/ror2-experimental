6266a364d6f2a1aa6ca95abef02afaee18d8d24e
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-10-13 13:31:05 +1100

    [sots-m] Bump version

--- sots-meridian/src/Plugin.cs
+++ sots-meridian/src/Plugin.cs
@@ -13,7 +13,7 @@ namespace MeridianPrimePrime
         public const string GUID = Author + "." + Name;
         public const string Author = "itsschwer";
         public const string Name = "MeridianPrimePrime";
-        public const string Version = "0.0.0";
+        public const string Version = "1.0.0";
 
         internal static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
 
