b7622694717819be6acaf619306019fff36e27c0
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-11-22 19:37:50 +1100

    [exp] Create PreventProfileSaving

---
+++ _experimental/src/Patches/PreventProfileSaving.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RoR2;
+
+namespace Experimental.Patches
+{
+    [HarmonyPatch]
+    internal static class PreventProfileSaving
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(SaveSystem), nameof(SaveSystem.Save))]
+        private static bool SaveSystem_Save(ref bool __result)
+        {
+            const bool dontSave = true;
+            __result = !dontSave;
+            return !dontSave;
+        }
+    }
+}
