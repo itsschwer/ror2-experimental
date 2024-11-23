9bab2df94f89be434737e8fba28b7a3740c481d3
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-11-23 15:22:05 +1100

    exp: Simplify NoDropPods to HarmonyPrefix

--- _experimental/src/Patches/NoDropPods.cs
+++ _experimental/src/Patches/NoDropPods.cs
@@ -1,6 +1,4 @@
-﻿using MonoMod.Cil;
-using Mono.Cecil.Cil;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RoR2;
 
 namespace Experimental.Patches
@@ -8,27 +6,11 @@ namespace Experimental.Patches
     [HarmonyPatch]
     internal static class NoDropPods
     {
-        [HarmonyILManipulator, HarmonyPatch(typeof(Run), nameof(Run.spawnWithPod), MethodType.Getter)]
-        private static void Run_spawnWithPod(ILContext il)
+        [HarmonyPrefix, HarmonyPatch(typeof(Run), nameof(Run.spawnWithPod), MethodType.Getter)]
+        private static bool Run_spawnWithPod(ref bool __result)
         {
-            ILCursor c = new ILCursor(il);
-            bool matched = c.TryGotoNext(
-                x => x.MatchCall<Run>($"get_{nameof(Run.instance)}"),
-                x => x.MatchLdfld<Run>(nameof(Run.stageClearCount)),
-                x => x.MatchLdcI4(0),
-                x => x.MatchCeq(),
-                x => x.MatchRet()
-            );
-
-            if (matched) {
-                c.RemoveRange(4);
-                c.Emit(OpCodes.Ldc_I4_0);
-
-#if DEBUG
-                Plugin.Logger.LogDebug(il.ToString());
-#endif
-            }
-            else Plugin.Logger.LogError($"{nameof(NoDropPods)}> Cannot hook {nameof(Run_spawnWithPod)}: failed to match IL instructions.");
+            __result = false;
+            return false;
         }
     }
 }
