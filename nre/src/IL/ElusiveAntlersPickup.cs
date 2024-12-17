14fdfe495081c5153a3bcdd16401eb89f36ccbd7
Schwer <42115696+itsschwer@users.noreply.github.com>
2024-12-17 22:55:28 +1100

    nre: Fix ElusiveAntlersPickup_FixedUpdate

--- nre/src/IL/ElusiveAntlersPickup.cs
+++ nre/src/IL/ElusiveAntlersPickup.cs
@@ -10,13 +10,30 @@ namespace itsschwer.RoR2.NRE.IL
     [HarmonyPatch]
     internal static class ElusiveAntlersPickup
     {
+        // Consistent NRE on client for each pickup spawn
+        // [Error  : Unity Log] NullReferenceException: Object reference not set to an instance of an object
+        // Stack trace:
+        // RoR2.ElusiveAntlersPickup.Start () (at <a43009bc6a5f4aee99e5521ef176a18d>:IL_0000)
+
+        // Inconsistent NRE on host on stage change
+        // [Error  : Unity Log] NullReferenceException
+        // Stack trace:
+        // UnityEngine.Component.GetComponent[T] () (at <a20b3695b7ce4017b7981f9d06962bd1>:IL_0021)
+        // RoR2.CharacterBody.CallRpcOnShardDestroyedClient (RoR2.ElusiveAntlersPickup+ElusiveAntlersDestroyCondition pickupCondition) (at <a43009bc6a5f4aee99e5521ef176a18d>:IL_003A)
+        // RoR2.CharacterBody.OnShardDestroyed (RoR2.ElusiveAntlersPickup+ElusiveAntlersDestroyCondition pickupCondition) (at <a43009bc6a5f4aee99e5521ef176a18d>:IL_0023)
+        // RoR2.ElusiveAntlersPickup.OnDestroy () (at <a43009bc6a5f4aee99e5521ef176a18d>:IL_0007)
+
+
         /// <summary>
+        /// Fixes vanilla NullReferenceException spam on host from Elusive Antlers pickups when the associated owner CharacterBody is destroyed (e.g. player death; starts when ragdoll destroyed?).
+        /// </summary>
+        /// <remarks>
         /// <code>
         /// [Error  : Unity Log] NullReferenceException
         /// Stack trace:
         /// RoR2.ElusiveAntlersPickup.FixedUpdate () (at <a43009bc6a5f4aee99e5521ef176a18d>:IL_003C)
         /// </code>
-        /// </summary>
+        /// </remarks>
         [HarmonyILManipulator, HarmonyPatch(typeof(Vanilla.ElusiveAntlersPickup), nameof(Vanilla.ElusiveAntlersPickup.FixedUpdate))]
         private static void ElusiveAntlersPickup_FixedUpdate(ILContext il)
         {
@@ -33,7 +50,7 @@ namespace itsschwer.RoR2.NRE.IL
             };
 
             if (c.TryGotoNext(match)) {
-                c.RemoveRange(match.Length); // :p
+                c.RemoveRange(match.Length-1); // :p (keep stloc.0)
                 c.Emit(OpCodes.Ldarg_0);
                 c.EmitDelegate<Func<Vanilla.ElusiveAntlersPickup, Vector3>>((@this) => {
                     if (@this.ownerBody) return @this.ownerBody.gameObject.transform.position;
--- nre/src/Plugin.cs
+++ nre/src/Plugin.cs
@@ -8,7 +8,7 @@ namespace itsschwer.RoR2.NRE
     {
         public const string GUID = Author + "." + Name;
         public const string Author = "itsschwer";
-        public const string Name = "PLUGIN_NAME";
+        public const string Name = "NREater";
         public const string Version = "0.0.0";
 
         internal static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
