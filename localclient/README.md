81426202ca755de783cb1b4016ca59304b87ec07
Schwer <42115696+itsschwer@users.noreply.github.com>
2025-11-25 10:26:24 +1100

    lc: Create

---
+++ localclient/README.md
@@ -0,0 +1,13 @@
+# Local Client
+
+Intended for developers to use on client profiles when testing multiplayer mods locally
+
+- prevents user profile saving
+    - *so that you do no have to switch between a main and a dev save*
+- unlocks the console so that it is easier to access
+    - *for `connect localhost:7777`*
+
+Also refer to: https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Testing/Testing-Multiplayer-Alone/
+
+## see also
+- [MultiplayerModTesting](https://thunderstore.io/package/Nuxlar/MultiplayerModTesting/) by [Nuxlar](https://thunderstore.io/package/Nuxlar/)
---
+++ localclient/src/Plugin.cs
@@ -0,0 +1,41 @@
+using BepInEx;
+using HarmonyLib;
+using RoR2;
+
+namespace LocalClient
+{
+    [HarmonyPatch]
+    [BepInPlugin(GUID, Name, Version)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = Author + "." + Name;
+        public const string Author = "itsschwer";
+        public const string Name = "localclient";
+        public const string Version = "0.0.0";
+
+        internal static new BepInEx.Logging.ManualLogSource Logger { get; private set; }
+
+        private void Awake()
+        {
+            // Use Plugin.GUID instead of Plugin.Name as source name
+            BepInEx.Logging.Logger.Sources.Remove(base.Logger);
+            Logger = BepInEx.Logging.Logger.CreateLogSource(Plugin.GUID);
+
+            new Harmony(Info.Metadata.GUID).PatchAll();
+
+            // Unlock console so it's easier to open and input "connect localhost:7777"
+            RoR2.UI.ConsoleWindow.cvConsoleEnabled.defaultValue = "1";
+            RoR2.UI.ConsoleWindow.cvConsoleEnabled.SetBool(true);
+
+            Logger.LogMessage("~awake.");
+        }
+
+
+        [HarmonyPrefix, HarmonyPatch(typeof(SaveSystem), nameof(SaveSystem.Save))]
+        private static bool SaveSystem_Save(ref bool __result)
+        {
+            __result = true; // pretend save was successful (so that pending save requests are not stuck trying to save every frame [SaveSysem.StaticUpdate])
+            return false;    // never run method
+        }
+    }
+}
